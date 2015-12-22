02a68e9b398ba90009ecaa61cca0f4609b76ccea
W.D <djvoleur@gmail.com>
2015-12-21 20:50:49 -0700

    Remove zerofltexx gps items Will come back to this
    Change-Id: Ide72da7e8b672b3d9f5aede4c6e04ad0ac31744d

--- device.mk
+++ device.mk
@@ -93,8 +93,6 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml \
-    $(LOCAL_PATH)/configs/lhd.conf:system/etc/lhd.conf
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
